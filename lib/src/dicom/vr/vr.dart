5ae3ce8aeb881da08526875b8bbc77737381fef4
Jim Philbin <jfphilbin@gmail.com>
2016-12-23 10:00:21 -0500

    Added VR.indexOf

--- lib/src/dicom/vr/vr.dart
+++ lib/src/dicom/vr/vr.dart
@@ -185,6 +185,9 @@ class VR {
   static VR lookup8(int vrCode) => vrs[vrCodeToIndex(vrCode)];
 
   /// Returns a [VR] if [vrCode](one [Uint16] integer) is a valid [VR] [name].
+  static int indexOf(int vrCode) => vrCodeToIndex(vrCode);
+
+  /// Returns a [VR] if [vrCode](one [Uint16] integer) is a valid [VR] [name].
   static VR lookup16(int vrCode) => vrs[vrCodeToIndex(vrCode)];
 
   //TODO: create invertedMap
