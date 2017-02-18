c061da067469045b636f3336198c2480a1ce8867
JFPhilbin <jfphilbin@gmail.com>
2017-02-18 09:55:18 -0500

    pubspec.yaml - changed dependency back to local file system

--- test/uid/uid_test.dart
+++ test/uid/uid_test.dart
@@ -4,8 +4,7 @@
 // Author: Jim Philbin <jfphilbin@gmail.edu> - 
 // See the AUTHORS file for other contributors.
 
-import 'package:dictionary/dictionary.dart';
-import 'package:test/test.dart';
+
 
 void main() {
   fooTest();
@@ -22,6 +21,18 @@ void fooTest() {
       expect(uid.asString, equals("1.2.840.10008.1.2"));
       uid = Uid.parse("1.2.840.10008.1.2.1");
       expect(uid == WKUid.kExplicitVRLittleEndian, true);
+
+    });
+
+    test('Bad String to UID should fail', () {
+      // Bad letter 'Z'
+      Uid uid = Uid.parse("1.2.8z0.10008.1.2");
+      expect(uid == null, true);
+      //TODO: this should return null or
+      uid = Uid.parse("4.2.840.10008.1.2");
+      print('uid: $uid');
+      expect(uid == null, true);
+
 
     });
 
