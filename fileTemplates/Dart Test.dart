7825d308774b63f52aae55cd6a41caf98c1c2bfa
JFPhilbin <jfphilbin@gmail.com>
2017-03-04 18:20:08 -0500

    WS-2016.3.3 <jim@JFP-T460s Create baseRefactoring.xml, IntelliLang.xml, git_toolbox.xml, ide.general.xml, git.xml, project.default.xml, diff.xml, other.xml, terminal.xml, applicationLibraries.xml, file.template.settings.xml, ignore.xml, Dart Class.dart, find.xml, ui.lnf.xml, filetypes.xml, keymap.xml, code.style.schemes.xml, colors.scheme.xml, notifications.xml, DICOMweb default.icls, Dart.xml, github_settings.xml, Dart Program.dart, Dart File.dart, gutter.xml, abbreviations.xml, Default.xml, laf.xml, path.macros.xml, intentionSettings.xml, Default _1_.xml, web-browsers.xml, vcs.xml, editor.codeinsight.xml, Dart Test.dart, editor.xml, Dart Pubspec.yaml, markdown_navigator.shared.xml, debugger.xml

---
+++ fileTemplates/Dart Test.dart
@@ -0,0 +1,27 @@
+// Copyright (c) 2016, Open DICOMweb Project. All rights reserved.
+// Use of this source code is governed by the open source license
+// that can be found in the LICENSE file.
+// Author: Binayak Behera <binayak.b@mwebware.com> -
+// See the AUTHORS file for other contributors.
+
+import 'package:dictionary/dictionary.dart';
+import 'package:core/core.dart';
+import 'package:test/test.dart';
+
+import 'dart:math';
+import 'dart:convert';
+import 'dart:typed_data';
+
+void main() {
+  TestingforElement();
+}
+
+fooTest() {
+  group("group description", () {
+    test("intger SS", () {
+      ... 
+      expect(exp, matcher(...));
+
+      });
+  });
+}
